a0c1f334747035cb1340494199cefe416988adca
Zhang Wentao <mail@zhang-wen-tao.com>
2023-12-02 05:51:31 +0800

    update

--- src/components/OutlineButton.svelte
+++
@@ -1,7 +0,0 @@
-<button
-  {...$$restProps}
-  class="rounded-md bg-transparent transition-colors duration-200 border border-gray-200 hover:border-gray-300 dark:border-gray-700 dark:bg-gray-700 text-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:border-gray-600 text-sm font-normal text-center px-4 h-10 flex items-center justify-center space-x-3"
-  on:click
->
-  <slot />
-</button>
